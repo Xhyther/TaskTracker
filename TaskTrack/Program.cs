5ed358278d20070f47ea763afa0b512e2eb40fae
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2025-01-15 01:39:45 +0800

    added some of the possible arguments to add and remove commands

--- TaskTrack/Program.cs
+++ TaskTrack/Program.cs
@@ -8,8 +8,8 @@ class Program
     {
        //Root Command
         var rootCommand = new RootCommand("task-cli");
-        var adddCommand = new Command("add", "Add a new task");
-        adddCommand.AddAlias("a");
+        var addCommand = new Command("add", "Add a new task");
+        addCommand.AddAlias("a");
         var listCommand = new Command("list", "List all tasks");
         listCommand.AddAlias("l");
         var removeCommand = new Command("remove", "Remove a task");
@@ -17,10 +17,30 @@ class Program
         var updateCommand = new Command("update", "Update a task");
         updateCommand.AddAlias("u");
 
-        rootCommand.AddCommand(adddCommand);
+        rootCommand.AddCommand(addCommand);
         rootCommand.AddCommand(listCommand);
         rootCommand.AddCommand(removeCommand);
         rootCommand.AddCommand(updateCommand);
+
+        //Defining the Argument for adding a Task
+        var TaskNameArgument = new Argument<string>
+        (name: "TaskName", 
+        description: "Name of the task");
+
+        var DeleteNameArgument = new Argument<string>
+        (name: "NameToDelete",
+        description: "Name of the task to delete");
+
+        var DeleteIDArgument = new Argument<int>
+        (name: "IDToDelete",
+        description: "ID of the task to delete");
+
+
+        //Adding a task to the add command
+        addCommand.AddArgument(TaskNameArgument);
+        removeCommand.AddArgument(DeleteNameArgument);
+        removeCommand.AddArgument(DeleteIDArgument);
+
 
 
 
