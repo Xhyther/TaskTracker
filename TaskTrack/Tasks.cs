76993e6b138ef4e1ae87f0dedc5d1825381cbae0
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2025-01-15 00:31:48 +0800

    Change the initialization of the fields through the constructors

--- TaskTrack/Tasks.cs
+++ TaskTrack/Tasks.cs
@@ -5,16 +5,24 @@ namespace TaskTrack;
 public class Tasks
 {
     private static int TaskId { get; set; } = 0;
-    private string TaskName { get; set; } = $"Task {TaskId}";
-    private string? TaskDescription { get; set; } = string.Empty;
-    private bool TaskCompleted { get; set; } = false;
-    private TaskState TaskState { get; set; } = TaskState.NotStarted;
-    private PriorityLevel TaskPriority { get; set; } = PriorityLevel.Normal;
-    private DateTime TaskDate { get; set; } = DateTime.Now;
+    private string TaskName { get; set; }
+    private string? TaskDescription { get; set; }
+    private bool TaskCompleted { get; set; }
+    private TaskState TaskState { get; set; }
+    private PriorityLevel TaskPriority { get; set; }
+    private DateTime TaskDate { get; set; }
 
-    
     public Tasks()
     {
         TaskId++;
+        TaskName = $"Task {TaskId}";
+        TaskDescription = string.Empty;
+        TaskCompleted = false;
+        TaskState = TaskState.NotStarted;
+        TaskPriority = PriorityLevel.Normal;
+        TaskDate = DateTime.Now;
     }
+
+    
+   
 }
