9330716c70ebfb6f2ef39dfd8f09337f6be09e92
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2025-01-15 02:12:34 +0800

    made some minor changes to the description

--- TaskTrack/Program.cs
+++ TaskTrack/Program.cs
@@ -16,8 +16,8 @@ class Program
         {
             new Argument<string> (name: "TaskName", description: "Name of the task"),
             new Argument<string> (name: "TaskDescription", description: "Description of the task"),
-            new Option<string>(new string[] {"--state", "-s"}, "State of the task"),
-            new Option<string>(new string[] {"--priority", "-p"}, "Priority of the task")
+            new Option<string>(new string[] {"--state", "-s"}, "State of the task (default: not started)"),
+            new Option<string>(new string[] {"--priority", "-p"}, "Priority of the task (default: low)")
         };
         addCommand.AddAlias("a");
 
@@ -36,8 +36,8 @@ class Program
             new Argument<int> (name: "TaskId", description: "ID of the task"),
             new Argument<string> (name: "TaskName", description: "Name of the task"),
             new Argument<string> (name: "TaskDescription", description: "Description of the task"),
-             new Option<string>(new string[] {"--state", "-s"}, "State of the task"),
-            new Option<string>(new string[] {"--priority", "-p"}, "Priority of the task")
+            new Option<string>(new string[] {"--state", "-s"}, "State of the task (default: not started)"),
+            new Option<string>(new string[] {"--priority", "-p"}, "Priority of the task (default: low)")
 
         };
         updateCommand.AddAlias("u");
